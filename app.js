b58060c1fe60306ef7b8beca45329954beb78170
AndrewAAG <andrewalexander.gun@gmail.com>
2025-12-02 15:32:16 +0700

    Student: Jadwal Bimbingan
    Buat frontend untuk student: jadwal bimbingan, masih menggunakan mockup belum integrasi dengan backend

--- app.js
+++ app.js
@@ -9,10 +9,16 @@ app.use(cors());
 // untuk parsing JSON
 app.use(express.json());
 // Untuk routing file HTML/CSS/JS static
-app.use(express.static('public')); 
+app.use(express.static('public', { extensions: ['html'] }));
+
+
 app.use('/api/auth', authRoutes);
 
 
+// Redirect Utama ke Login Page
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
