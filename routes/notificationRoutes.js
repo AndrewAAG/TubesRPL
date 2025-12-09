3ee6afba6135dff05f39147fa66cf782494a3d8e
MATTHEW-ANDERS <6182301022@student.unpar.ac.id>
2025-12-09 13:24:13 +0700

    Update notificationRoutes.js

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -5,5 +5,8 @@ const notificationController = require('../controllers/notificationController');
 // Route untuk mengambil notifikasi berdasarkan user_id
 // URL Akhir: /api/notifications?user_id=...
 router.get('/', notificationController.getUserNotifications);
+router.get('/count', notificationController.getUnreadCount);
+router.post('/mark-read', notificationController.markAsRead);
+router.post('/broadcast', notificationController.sendBroadcast);
 
 module.exports = router;
